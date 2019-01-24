b0078aa1bf6f710d1e7df2e8c63ec81269b45838
JJack27 <yizhouzhao27@gmail.com>
2019-01-23 23:21:46 -0700

    finished first 3 questions

--- answer.md
+++ answer.md
@@ -2,4 +2,7 @@
 - We can inspect all environment variables in python my `import os` and use `os.environ`. `os.environ` will return a dictionary-like class (\<class 'os._Environ'\>) with all environment variables as keys and their values.
 
 ## Question 2
-- 
+- `QUERY_STRING` contains information about query in `URLs`
+
+## Question 3
+- `HTTP_USER_AGENT` contains information about user's browser.
--- hello.py
+++ hello.py
@@ -1,10 +1,39 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 import os
+from pprint import pprint
+import json
+from templates import *
+
+from urllib.parse import parse_qs
+import sys
 
 def main():
-    length = os.environ['CONTENT_LENGTH']
-    cookie = os.environ['HTTP_COOKIE']
+    print("Content-Type: text/html")
+    print()
+    
+    print("<!doctype html>")
+    print("<head>")
+    print("<title>Hello</title>")
+    print("<style>pre {white-space: pre-wrap; word-wrap: break-word;}</style>")
+    print("</head>")
+    print("<h2>Hello</h2>")
 
-    print(length)
+    print("<dl>")
+    print("<dt>QUERY_STRING:</dt>")
+    print("<dd>")
+    print(parse_qs(os.environ.get("QUERY_STRING")))
+    print("</dd>")
+    print("<dt>HTTP_USER_AGENT:</dt>")
+    HTTP_USER_AGENT = os.environ.get("HTTP_USER_AGENT", None)
+    print("<dd>" + HTTP_USER_AGENT + "</dd>")
+    print("</dl>")
+
+    print("<pre>")
+    env = {}
+    for key, value in os.environ.items():
+        env[key] = value
+    print(json.dumps(env))
+    print("</pre>")
+    
 
 main()
